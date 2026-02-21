cb5a6ac9f3d2671465f5de026928020db62874cc
MarGreenDev <marylougroeneveld@gmail.com>
2026-02-21 20:47:43 +0100

    Add test message on ready event

--- src/index.js
+++ src/index.js
@@ -9,6 +9,8 @@ const botToken = process.env.DISCORD_TOKEN;
 const client = new Client({intents: [GatewayIntentBits.Guilds]});     // Guild intent is needed for basic server access and slash commands
 
 client.on("clientReady", () => {    // Runs when the bot is connected to Discord
+    client.channels.cache.get('1474789551993000078')
+    .send("hi, I'm online :)")
     console.log("Bot is online");
 })
 
